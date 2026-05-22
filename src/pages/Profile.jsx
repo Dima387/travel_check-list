8855f9f826a42918c3d39cdbb569b492f370cb71
fiuari <fiuari.federico@gmail.com>
2026-05-22 19:08:52 +0300

    feat: наведено красу в коді, додано відступи та пропуски рядків для кращої читабельності

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,39 +4,15 @@ export default function Profile(){
 
 return(
 
-<MainLayout>
-
-<div
-className="
-flex
-gap-8
-"
->
-
-<div
-className="
-w-40
-h-40
-bg-slate-700
-rounded-full
-"/>
-
-<div>
-
-<h1>
-Username
-</h1>
-
-<p>
-user@email.com
-</p>
-
-</div>
-
-</div>
-
-</MainLayout>
+    <MainLayout>
+        <div className="flex gap-8">
+            <div className="w-40 h-40 bg-slate-700 rounded-full"/>
+            <div>
+                <h1>Username</h1>
+                <p>user@email.com</p>
+            </div>
+        </div>
+    </MainLayout>
 
 )
-
 }
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -4,45 +4,13 @@ export default function Wishlist(){
 
 return(
 
-<MainLayout>
-
-<h1
-className="
-text-4xl
-mb-6
-"
->
-
-Wishlist
-
-</h1>
-
-<div
-className="
-grid
-grid-cols-2
-gap-5
-"
->
-
-<div
-className="
-bg-slate-900
-h-48
-rounded-xl
-"/>
-
-<div
-className="
-bg-slate-900
-h-48
-rounded-xl
-"/>
-
-</div>
-
-</MainLayout>
+    <MainLayout>
+        <h1 className="text-4xl mb-6">Wishlist</h1>
+        <div className="grid grid-cols-2 gap-5">
+            <div className="bg-slate-900 h-48 rounded-xl"/>
+            <div className="bg-slate-900 h-48 rounded-xl"/>
+        </div>
+    </MainLayout>
 
 )
-
 }
