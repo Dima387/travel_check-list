32b97b4f3ce46839783003fa4dc65ce2fb576f6c
Dmytro Kolesnikov <kdmitro466@gmail.com>
2026-05-23 09:39:09 +0300

    Adjusted the logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom"
-import logo from "../assets/logo.png"
+import logo from "../../public/img/logo.png"
 
 export default function Header(){
 
 return(
 
     <header className="bg-slate-950 h-16 flex justify-between items-center px-8">
-        <Link to="/" className="flex items-center gap-2">
-            <img src={logo} alt="TravelMap Logo" className="h-14 w-auto object-contain" />
+        <Link to="/" className="flex items-center">
+            <img src={logo} alt="TravelMap" className="w-10 h-10 object-contain"/>
+            <span className="text-2xl font-bold text-cyan-400">TravelMap</span>
         </Link>
-        <input placeholder="Search" className="bg-slate-800 rounded-lg px-4 py-2"/>
+        <input placeholder="Search" className=" bg-slate-800 rounded-lg px-4 py-2"/>
     </header>
 
 )
